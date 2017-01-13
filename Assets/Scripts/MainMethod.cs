5c089f1320c2e25fd5cca69d370448e2eec5cfd9
Limdull <leconte555@laposte.net>
2017-01-13 22:38:34 +0100

    Ajout du système du message du jour
    V0.5 du message du jour

--- Assets/Scripts/MainMethod.cs
+++ Assets/Scripts/MainMethod.cs
@@ -3,14 +3,34 @@ using System.Collections;
 
 public class MainMethod : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    bool aboutusbox = false;
+    bool mainmenu  = true;
+ 
+    public void OnGUI()
+    {
+        if (mainmenu)
+        {
+            if (GUI.Button(new Rect(0, 0, Screen.width, Screen.height), "Bonjour superviseur"))
+            {
+                aboutusbox = true;
+                mainmenu = false;
+            }
+        }
+        if (aboutusbox)
+        {
+            GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "Aujourd'hui , voud devrez surveillez les mots traitres et les absences au travail");
+            
+        }
+
+    }
 
 }
